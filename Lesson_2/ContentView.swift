827d4f267348194c927af8b0426dddba7276a9af
Igor Polousov <igorpolousov@gmail.com>
2022-04-28 16:20:41 +0300

    Initial Commit

---
+++ Lesson_2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lesson_2
+//
+//  Created by Igor Polousov on 28.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lesson_2/Lesson_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_2App.swift
+//  Lesson_2
+//
+//  Created by Igor Polousov on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
